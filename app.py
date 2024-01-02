f193558ba7a5dafd2cd00e5ff6f53fe04bbb056f
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-01-02 02:45:28 -0500

    python code formatted

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask,render_template ,request, redirect,session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_session import Session
 from datetime import date
 from flask_sqlalchemy import SQLAlchemy
@@ -8,21 +8,22 @@ import sqlite3
 from datetime import date
 import helpers
 
-#env variables
+# env variables
 load_dotenv()
-#address API
+# address API
 
-api = os.getenv('SECRET_KEY')
+api = os.getenv("SECRET_KEY")
 
-#configure application
+# configure application
 app = Flask(__name__)
 
-#making db
-# basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
-#\'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# making db
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     review_date = db.Column(db.Date, default=date.today())
@@ -36,13 +37,14 @@ class Reviews(db.Model):
     review = db.Column(db.String(150), nullable=True)
 
 
-#Configure session to use filesystem (instead of signed cookies)
+# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 Session(app)
 
-app.secret_key = os.getenv('SESSION_KEY') #session key
+app.secret_key = os.getenv("SESSION_KEY")  # session key
+
 
 @app.after_request
 def after_request(response):
@@ -53,32 +55,39 @@ def after_request(response):
     return response
 
 
-@app.route("/",methods=["GET"])
+# homepage
+@app.route("/", methods=["GET"])
 def index():
     try:
-        if request.method=="GET":
-            return render_template("index.html",api=api)
+        if request.method == "GET":
+            return render_template("index.html", api=api)
         else:
             print("Error(index): POST")
     except Exception as e:
         print(f"Error is {e}")
 
+
+# checking the address from the database
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
-    
     try:
-        if request.method=="GET":
+        if request.method == "GET":
             return redirect("/")
         elif request.method == "POST":
             address = (request.form.get("address")).strip().title()
-            print(address)      
-            input_search = Reviews.query.filter_by(address=address).order_by(Reviews.id.desc()).all()
+            print(address)
+            input_search = (
+                Reviews.query.filter_by(address=address)
+                .order_by(Reviews.id.desc())
+                .all()
+            )
             input_search = [review.__dict__ for review in input_search]
-            
-            session["address"]= address
+
+            session["address"] = address
             session["input_search"] = input_search
-            # a = session["address"]
-            return render_template("result.html",input_search=input_search,a=session["address"])
+            return render_template(
+                "result.html", input_search=input_search, a=session["address"]
+            )
         else:
             return redirect("/")
     except Exception as e:
@@ -86,12 +95,13 @@ def submit():
         return redirect("/")
 
 
-@app.route("/add",methods=["GET","POST"]) 
+# form for adding a new review
+@app.route("/add", methods=["GET", "POST"])
 def add():
     try:
-        if request.method=="POST":
+        if request.method == "POST":
             address = session.get("address")
-            return render_template("add.html",address=address,api=api)
+            return render_template("add.html", address=address, api=api)
         else:
             return redirect("/")
     except Exception as e:
@@ -99,35 +109,37 @@ def add():
         return redirect("/")
 
 
-#route for a new review submit
-@app.route("/new_review",methods=["GET","POST"])
+# route triggered after the submit button on the new review form
+@app.route("/new_review", methods=["GET", "POST"])
 def new_review():
     try:
         if request.method == "POST":
-            form_data = {"address":request.form.get("address"),
-            "rent":request.form.get("rent"),
-            "years":request.form.get("years"),
-            "clean":request.form.get("clean"),
-            "maintain": request.form.get("maintain"),
-            "amenities" : request.form.get("amenities"),
-            "neighbourhood": request.form.get("neighbourhood"),
-            "review": request.form.get("review")
+            form_data = {
+                "address": request.form.get("address"),
+                "rent": request.form.get("rent"),
+                "years": request.form.get("years"),
+                "clean": request.form.get("clean"),
+                "maintain": request.form.get("maintain"),
+                "amenities": request.form.get("amenities"),
+                "neighbourhood": request.form.get("neighbourhood"),
+                "review": request.form.get("review"),
             }
             print(form_data)
             data_checked = helpers.check_data(form_data)
             # print(data_checked)
             if data_checked:
-                data_prepared= helpers.prepare_data(form_data)
+                data_prepared = helpers.prepare_data(form_data)
                 print(data_prepared)
                 insert_response = insert_data(data_prepared)
-                
+
                 if insert_response:
                     print("200")
                 else:
-                    print("insert is false")
+                    # not completed
                     print("501")
                 return redirect("/")
             else:
+                # not completed
                 print("501")
             return redirect("/")
         else:
@@ -136,57 +148,56 @@ def new_review():
     except Exception as e:
         print(f"Exception is: {e}")
         return redirect("/")
-#for signing up
-@app.route('/signup')
+
+
+# for signing up
+@app.route("/signup")
 def signup():
-    return render_template('wip.html')
+    return render_template("wip.html")
 
-#for logging
-@app.route('/login')
+
+# for logging
+@app.route("/login")
 def login():
-    return render_template('wip.html')
+    return render_template("wip.html")
 
-#for inserting data
+
+# for inserting data
 def insert_data(data_prepared):
     try:
-        # connection = sqlite3.connect("database.db")
-        # cursor = connection.cursor()
-        # sql_query = "INSERT INTO REVIEWS (address,rent,years,clean,maintain, amenities, neighbourhood, review) VALUES (?,?,?,?,?,?,?,?)"
-        # values = tuple(value for key,value in data_prepared.items())
-        # cursor.execute(sql_query,values)
-        # connection.commit()
-        # connection.close()
         print(data_prepared)
         new_review = Reviews(
-        address=data_prepared["address"],
-        rent=data_prepared["rent"],
-        years=data_prepared["years"],
-        clean=data_prepared["clean"],
-        maintain=data_prepared["maintain"],
-        amenities=data_prepared["amenities"],
-        neighbourhood=data_prepared["neighbourhood"],
-        review=data_prepared["review"]
+            address=data_prepared["address"],
+            rent=data_prepared["rent"],
+            years=data_prepared["years"],
+            clean=data_prepared["clean"],
+            maintain=data_prepared["maintain"],
+            amenities=data_prepared["amenities"],
+            neighbourhood=data_prepared["neighbourhood"],
+            review=data_prepared["review"],
         )
-        #adding to the db
-        db.session.add(new_review)  
+        # adding to the db
+        db.session.add(new_review)
         # Commit the changes to the database
         db.session.commit()
         return True
     except Exception as e:
-        print(f"e IS {e}")
+        print(e)
         db.session.rollback()
         return False
 
-#for 404 errors
+
+# errorhandlers
 @app.errorhandler(404)
 def method_not_allowed(error):
     return render_template("404.html")
+
 
 @app.errorhandler(405)
 def method_not_allowed(error):
     return redirect("/")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     command = "gunicorn -w 4 -b 0.0.0.0:5500 -t 60 your_app_module:app"
     app.run(debug=True)
-
--- helpers.py
+++ helpers.py
@@ -1,78 +1,118 @@
 import re
 
+# regex for valid address pattern
+pattern = re.compile(r"""^(?=.*\d)[a-zA-Z0-9, "']+$""")
 
-#checking valid address
-pattern = re.compile(r'''^(?=.*\d)[a-zA-Z0-9, "']+$''') 
 
 def main():
-  try:
-    result_data = check_data(form_data={'address': '34 Keon Pl, Scarborough, Toronto, On M1B 3C9 Can', 'rent': None, 'years': None, 'clean': 'Dirty', 'maintain': 'Needs Improvement', 'amenities': 'Lacking', 'neighbourhood': 'Unfavorable', 'review': ''})
-    print(result_data)
-    if result_data:
-      final_data = prepare_data(form_data={'address': '34 Keon Pl, Scarborough, Toronto, On M1B 3C9 Can', 'rent': None, 'years': None, 'clean': 'Dirty', 'maintain': 'Needs Improvement', 'amenities': 'Lacking', 'neighbourhood': 'Unfavorable', 'review': ''})
-    print(final_data)
-  except Exception as e:
-    print(e)
-    return False
+    # sample data passed to the functions
+    try:
+        result_data = check_data(
+            form_data={
+                "address": "34 Keon Pl, Scarborough, Toronto, On M1B 3C9 Can",
+                "rent": None,
+                "years": None,
+                "clean": "Dirty",
+                "maintain": "Needs Improvement",
+                "amenities": "Lacking",
+                "neighbourhood": "Unfavorable",
+                "review": "",
+            }
+        )
+        # printing the return value
+        print(result_data)
+        if result_data:
+            final_data = prepare_data(
+                form_data={
+                    "address": "34 Keon Pl, Scarborough, Toronto, On M1B 3C9 Can",
+                    "rent": None,
+                    "years": None,
+                    "clean": "Dirty",
+                    "maintain": "Needs Improvement",
+                    "amenities": "Lacking",
+                    "neighbourhood": "Unfavorable",
+                    "review": "",
+                }
+            )
+        # printing the return value
+        print(final_data)
+    except Exception as e:
+        print(e)
+        return False
 
+
+# checking the data
 def check_data(form_data):
-  try:
-    for key,value in (form_data).items():
-      if key=="address":
-        if len((form_data["address"]).strip())<=10:
-          return False
-        elif not pattern.match(form_data[key]):
-          return False
-      elif key in ("rent","years"):
-        if not form_data[key]:
-          continue
-        elif float(form_data[key])<0:
-          return False
-      elif key=="clean":
-        if (form_data[key]).strip().title() not in ("Dirty","Average","Clean"):
-          return False
-      elif key=="maintain":
-        if (form_data[key]).strip().title() not in ("Needs Improvement","Adequate","Well Maintained"):
-          return False
-      elif key=="amenities":
-        if (form_data[key]).strip().title() not in ("Lacking","Basic","Good"):
-          return False
-      elif key=="neighbourhood":
-        if (form_data[key]).strip().title() not in ("Unfavorable","Okay","Safe"):
-          return False
-      elif key=="review":
-        if (len(form_data["review"])>150):
-          return False
-      
-    return True
-  except Exception as e:
-    return False
+    try:
+        for key, value in (form_data).items():
+            if key == "address":
+                if len((form_data["address"]).strip()) <= 10:
+                    return False
+                elif not pattern.match(form_data[key]):
+                    return False
+            elif key in ("rent", "years"):
+                if not form_data[key]:
+                    continue
+                elif float(form_data[key]) < 0:
+                    return False
+            elif key == "clean":
+                if (form_data[key]).strip().title() not in (
+                    "Dirty",
+                    "Average",
+                    "Clean",
+                ):
+                    return False
+            elif key == "maintain":
+                if (form_data[key]).strip().title() not in (
+                    "Needs Improvement",
+                    "Adequate",
+                    "Well Maintained",
+                ):
+                    return False
+            elif key == "amenities":
+                if (form_data[key]).strip().title() not in ("Lacking", "Basic", "Good"):
+                    return False
+            elif key == "neighbourhood":
+                if (form_data[key]).strip().title() not in (
+                    "Unfavorable",
+                    "Okay",
+                    "Safe",
+                ):
+                    return False
+            elif key == "review":
+                if len(form_data["review"]) > 150:
+                    return False
+
+        return True
+    except Exception as e:
+        return False
+
 
 def prepare_data(form_data):
-  for key,value in (form_data).items():
-    if key in ["rent","years"]:
-      if not form_data[key]:
-        form_data[key]=""
-      else:
-        form_data[key] = str(form_data[key])
+    for key, value in (form_data).items():
+        if key in ["rent", "years"]:
+            if not form_data[key]:
+                form_data[key] = ""
+            else:
+                form_data[key] = str(form_data[key])
+    # formatting the data in a form of dictionary
+    form_data = {
+        "address": form_data["address"].strip().title(),
+        "rent": form_data["rent"],
+        "years": form_data["years"],
+        "clean": form_data["clean"].strip().title(),
+        "maintain": form_data["maintain"].strip().title(),
+        "amenities": form_data["amenities"].strip().title(),
+        "neighbourhood": form_data["neighbourhood"].strip().title(),
+        "review": form_data["review"].strip().capitalize(),
+    }
 
-  form_data = {
-    'address': form_data["address"].strip().title(),
-    'rent': form_data["rent"],
-    'years': form_data["years"],
-    'clean': form_data["clean"].strip().title(),
-    'maintain': form_data["maintain"].strip().title(), 
-    'amenities': form_data["amenities"].strip().title(), 
-    'neighbourhood': form_data["neighbourhood"].strip().title(), 
-    'review': form_data["review"].strip().capitalize()
-  }
-  
-  #checking again
-  if check_data(form_data):
-    return form_data
-  else:
-    return False
-    
+    # checking again
+    if check_data(form_data):
+        return form_data
+    else:
+        return False
+
 
 if __name__ == "__main__":
-  main()
+    main()
