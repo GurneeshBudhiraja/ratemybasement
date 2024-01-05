d4c4648166dd57cc35007c865a9c1c8e8b97132d
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-01-05 04:40:13 -0500

    layout changed

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,16 @@
 const likeButton = document.querySelectorAll('.material-symbols-outlined');
 let cross = document.querySelector('.cross');
-let hamburgerMenu = document.querySelector('.hamburger-menu')
-let menuButton = document.querySelector('.menu')
-let crossClickable= false;
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+let menuButton = document.querySelector('.menu');
+let crossClickable = false;
+const sentences = ["Real Reviews for Real Renters.","Choose the Right Rental.","Choose Better with Renter Reviews.","Shape Our Community with Your Reviews."];
+let typingElement = document.querySelector('#index-motto'); // typing effect div
+const searchButton = document.querySelector('#index-search-review');
+// index page elements
+let searchButtonClicked = false; //to check whether button is clicked or not
+const typingText = document.querySelector('#typing-text');
+const onImage = document.querySelector('#on-image');
+const onHouse = document.querySelector('#on-house');
 
 // like button feature
 for (const button of likeButton) {
@@ -25,10 +33,43 @@ menuButton.addEventListener('click',()=> {
     crossClickable=true;
   }
 });
-
+// removing the menu 
 cross.addEventListener('click',()=>{
   if (crossClickable) {
     hamburgerMenu.style.display = 'none';
     crossClickable=false;
+  }
+});
+
+
+// typewriter effect
+var typewriter = new Typewriter(typingElement, {
+    loop: true,
+    autoStart: true,
+    delay:75
+});
+for (sentence of sentences) {
+  typewriter.typeString(sentence)
+      .pauseFor(150)
+      .deleteAll()
+      .start()
+}
+
+// for displaying the search bar
+searchButton.addEventListener('click',()=> {
+  if (!searchButtonClicked) {
+    typingText.style.display='none';
+    onImage.style.top='10%';
+    onImage.style.left='5%';
+    onHouse.style.display='flex';
+    searchButton.style.border="0.1rem solid black";
+    searchButtonClicked=true;
+  } else {
+    typingText.style.display='flex';
+    onImage.style.top='25%';
+    onImage.style.left='8%';
+    searchButton.style.border="none";
+    onHouse.style.display='none';
+    searchButtonClicked=false;
   }
 });
